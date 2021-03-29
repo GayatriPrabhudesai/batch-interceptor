a10683206a2a7ca21eea35b02d0c7c917d24f155
GayatriPrabhudesai <44767496+GayatriPrabhudesai@users.noreply.github.com>
2021-03-29 08:48:46 +0530

    Create interceptor.js

---
+++ src/interceptor.js
@@ -0,0 +1,7 @@
+function batchInterceptor(instance) { 
+instance.interceptors.request.use(request => { 
+// Add your code here 
+return request; 
+}, error => Promise.reject(error)); 
+} 
+export default batchInterceptor; 
