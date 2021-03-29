4e7412a0f7d590fa061de5d68d74735b4cc9d260
GayatriPrabhudesai <44767496+GayatriPrabhudesai@users.noreply.github.com>
2021-03-29 10:18:56 +0530

    Update interceptor.js

--- src/interceptor.js
+++ src/interceptor.js
@@ -1,7 +1,28 @@
-function batchInterceptor(instance) { 
-instance.interceptors.request.use(request => { 
-// Add your code here 
-return request; 
-}, error => Promise.reject(error)); 
-} 
+let requestList = [];
+function batchInterceptor(instance) {
+instance.interceptors.request.use(
+  (request) => requestHandler(request), 
+  (error) =>errorHandler(error)
+); 
+instance.interceptors.response.use(
+  (response) => responsehandler(response), 
+  (error) =>errorHandler(error)
+); 
+}
+function requestHandler(request) {
+  requestList.push(request);
+  _.debounce( batchRequest(requestList), 100 );
+}
+function responsehandler(response) {
+  if (response.length > 0) {
+    return response;
+  }
+  errorHandler("No records found!");
+}
+function errorHandler(error) {
+ return Promise.reject(error);
+}
+function batchRequest(requests) {
+  return requests;
+}
 export default batchInterceptor; 
