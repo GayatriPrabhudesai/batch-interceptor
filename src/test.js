9289a636707fb005c931386c5a60c0cfc7f1930b
GayatriPrabhudesai <44767496+GayatriPrabhudesai@users.noreply.github.com>
2021-03-29 09:46:45 +0530

    Update test.js

--- src/test.js
+++ src/test.js
@@ -2,9 +2,10 @@ function runTest() {
 const batchUrl = “/file-batch-api”; 
 
 // Should return [{id:”fileid1”},{id:”fileid2”}] 
-apiClient.get(batchUrl, {params: {ids: [“fileid1”,”fileid2”]}}); 
+let request1 = apiClient.get(batchUrl, {params: {ids: [“fileid1”,”fileid2”]}}); 
 // Should return [{id:”fileid2”}] 
-apiClient.get(batchUrl, {params: {ids: [“fileid2”]}}); 
+let request2 = apiClient.get(batchUrl, {params: {ids: [“fileid2”]}}); 
 // Should reject as the fileid3 is missing from the response apiClient.get(batchUrl, {params: {ids: [“fileid3”]}}); 
+let request3 = apiClient.get(batchUrl, {params: {ids: [“fileid3”]}}); 
 } 
 runTest();
