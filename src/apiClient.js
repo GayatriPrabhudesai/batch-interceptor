f54451a3a6cadfbcc255de6996a27483c5b28d3a
GayatriPrabhudesai <44767496+GayatriPrabhudesai@users.noreply.github.com>
2021-03-29 10:49:18 +0530

    Update apiClient.js

--- src/apiClient.js
+++ src/apiClient.js
@@ -6,6 +6,12 @@ host:
 “https://europe-west1-quickstart-1573558070219.cloudfunctions.net”, baseAPI: 
 “https://europe-west1-quickstart-1573558070219.cloudfunctions.net”, headers: {}
 const instance = axios.create(config); 
-batchInterceptor(instance); 
+let requests = batchInterceptor(instance);
+axios.all(requests).then((response) => {
+ return response;
+  // use/access the results 
+}).catch(error => {
+  return error;
+})
 } 
 export default client; 
